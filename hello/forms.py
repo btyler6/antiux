39a64125aafb4306a93667a19d3e70423b041070
btyler6 <blt6fr@virginia.edu>
2020-10-02 22:59:59 -0400

    form with placeholders

--- hello/forms.py
+++ hello/forms.py
@@ -16,6 +16,17 @@ class DonationForm(forms.ModelForm):
             'city', 
             'state', 
             )
+        widgets = {
+            'name' : forms.TextInput(attrs={'placeholder': 'please include your first and last name'}),
+            'address' : forms.TextInput(attrs={'placeholder': 'please enter your street address'}),
+            'phone' : forms.TextInput(attrs={'placeholder': 'please use the format: +x(xxx)xxx-xxxx to give your phone number and do not include any spaces'}), 
+            'amount' : forms.TextInput(attrs={'placeholder': 'please enter your donation amount and do not include any special characters or spaces'}),
+            'country' : forms.TextInput(attrs={'placeholder': 'please provide the country you live in'}), 
+            'zipcode' : forms.TextInput(attrs={'placeholder': 'please use for format: xxxxx-xxxx to provide your zip code'}),
+            'date' : forms.TextInput(attrs={'placeholder': 'please provide 10 characters for today\'s date i.e. 01/10/2020'}),  
+            'city' : forms.TextInput(attrs={'placeholder': 'plase provide the city that you live in'}), 
+            'state' : forms.TextInput(attrs={'placeholder': 'please provide state initials i.e. NY'}),
+        }
 """     def clean_zipcode(self):
         data = self.cleaned_data['zipcode']
         if data[5] != '-':
